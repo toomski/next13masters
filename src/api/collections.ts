9c1dc01499609e8ad8dc4c709159699b23b68c75
Tomasz Mitorski <mitorski@gmail.com>
2023-10-29 20:41:45 +0100

    up

--- src/api/collections.ts
+++ src/api/collections.ts
@@ -6,12 +6,17 @@ import {
 } from "@/gql/graphql";
 
 export const getCollectionsList = async () => {
+
 	const graphqlResponse = await executeGraphql(CollectionsGetListDocument, {});
 
 	return graphqlResponse.collections;
+
 };
 
 export const getCollectionBySlug = async ({ slug }: CollectionGetBySlugQueryVariables) => {
+	
 	const graphqlResponse = await executeGraphql(CollectionGetBySlugDocument, { slug });
+	
 	return graphqlResponse.collections[0] || null;
+
 };
